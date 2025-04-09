d096a48425f9e4d6e04a307efa91dde388b289a5
Freatercs <mrmade6@gmail.com>
2025-04-09 17:41:50 +0300

    zadanie

--- src/Arrays.js
+++ src/Arrays.js
@@ -3,18 +3,32 @@
   Преобразуте строку в массив чисел, отсортируйте их по возрастанию и верните строку с упорядоченными числами,
   разделенными запятыми.
 */
-export function arraySorting(data) {}
+export function arraySorting(data) {
+  const numbersArray = data.split(',').map(Number);
+  numbersArray.sort((a, b) => a - b);
+  return numbersArray.join(',');
+}
 
 /*
   В функцию arrayFiltering() приходит массив целых чисел.
   Преобразуйте массив таким образом, чтобы все элементы массива, в которых содержатся числа более 100,
   были бы удалены из массива, и верните его в качестве результата функции.
 */
-export function arrayFiltering(data) {}
+export function arrayFiltering(data) {
+  return data.filter((number) => number <= 100);
+}
 
 /*
   В функцию arrayPushing() приходят два массива целых чисел размерностью 5 элементов.
   Получите третий массив размерностью 10 элементов, в котором последовательно чередуются
   значения из первых двух массивов, и верните его в качестве результата функции.
 */
-export function arrayPushing(array1, array2) {}
+export function arrayPushing(array1, array2) {
+  const resultArray = [];
+    // Проходим по индексам массивов (предполагаем, что оба массива имеют длину 5)
+    for (let i = 0; i < 5; i++) {
+        resultArray.push(array1[i]); // Добавляем элемент из первого массива
+        resultArray.push(array2[i]); // Добавляем элемент из второго массива
+    }
+    return resultArray;
+}
